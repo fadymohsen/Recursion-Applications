bae1b1d46e7b06e05889b08b167ca05123153d00
fadymohsen <82896079+fadymohsen@users.noreply.github.com>
2022-11-07 15:20:07 +0200

    Create recursion.h

---
+++ recursion.h
@@ -0,0 +1,26 @@
+#ifndef RECURSION_RECURSION_H
+#define RECURSION_RECURSION_H
+#include <string>
+using namespace std;
+
+
+
+// (1) Printing Stars
+void printStars(int n);
+
+//------------------------------------------
+
+// (2) Power Function
+int powerNO(int base, int exp);
+
+//------------------------------------------
+
+// (3) Palindrome Function
+bool isPalindrome(string text);
+
+//------------------------------------------
+
+
+
+
+#endif //RECURSION_RECURSION_H
